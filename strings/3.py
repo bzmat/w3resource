885954b856833c0c59e30248ee5632d9eac6019c
bzmat <jedi.mattai@gmail.com>
2019-05-06 22:29:44 +0200

    add loops directory

--- strings/3.py
+++ strings/3.py
@@ -1,5 +1,16 @@
 # write a Python program to get a string made of the first 2 and the last 2 chars from a given string.
 # If the strinf length is less than 2, return instead of empty string.
 
-def new_string(string):
+def first_and_last(string):
+    if len(string) < 2:
+        new_string = ""
+    else:
+        new_string = string[0] + string[1] + string[-2] + string[-1]
+    return new_string
+
+# test
+print(first_and_last("mateusz"))
+print(first_and_last("wc"))
+print(first_and_last("m "))
+
 
